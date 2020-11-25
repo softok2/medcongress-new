53fb101a192a683dd475a99e6286eead60b8da39
Dennis Molinet <dennis.molinetg@gmail.com>
2020-11-25 15:15:55 -0500

    actualizacion

--- MedCongress/MedCongressApp/views.py
+++ MedCongress/MedCongressApp/views.py
@@ -47,7 +47,7 @@ ID_KEY='m6ftsapwjvmo7j7y8mop'
 PUBLIC_KEY='pk_0d4449445a4948899811cea14a469793'
 PRIVATE_KEY='sk_34664e85b5504ca39cc19d8f9b8df8a2'
 URL_API='sandbox-api.openpay.mx'
-URL_SITE='http://localhost:8000'
+URL_SITE='https://medcongress.softok2.mx'
 URL_PDF='sandbox-dashboard.openpay.mx'
 
 # ID_KEY='muq0plqu35rnjyo7sf2v'
