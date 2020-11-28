586d68cf2fc131ffc9641740370c8268b52ed4e0
Dennis Molinet <dennis.molinetg@gmail.com>
2020-11-28 00:06:05 -0500

    actualizar

--- MedCongress/MedCongressApp/views.py
+++ MedCongress/MedCongressApp/views.py
@@ -48,7 +48,8 @@ ID_KEY='m6ftsapwjvmo7j7y8mop'
 PUBLIC_KEY='pk_0d4449445a4948899811cea14a469793'
 PRIVATE_KEY='sk_34664e85b5504ca39cc19d8f9b8df8a2'
 URL_API='sandbox-api.openpay.mx'
-URL_SITE='http://localhost:8000'
+URL_SITE='https://medcongress.softok2.mx'
+# URL_SITE='http://localhost:8000'
 URL_PDF='sandbox-dashboard.openpay.mx'
 
 # ID_KEY='muq0plqu35rnjyo7sf2v'
@@ -191,7 +192,7 @@ class Perfil(TemplateView):
     def get_context_data(self, **kwargs):
         
         context = super().get_context_data(**kwargs)
-        congresos=RelCongresoUser.objects.filter(user=self.request.user.perfilusuario,is_pagado=True).distinct('congreso')
+        congresos=RelCongresoUser.objects.filter(user=self.request.user.perfilusuario,is_pagado=True).distinct('congreso','uuid_factura')
         context['congresos']=congresos
         congresos_pendientes=RelCongresoUser.objects.filter(user=self.request.user.perfilusuario,is_pagado=False).distinct('congreso')
         context['congresos_pendientes']=congresos_pendientes
