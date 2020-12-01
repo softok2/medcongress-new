4f618a3572a389c8dc19dbad836ac0487e3055db
amorell <amorell.cu@gmail.com>
2020-12-01 07:58:51 -0600

    configuracion de backend

---
+++ MedCongress/MedCongressApp/backends.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+User = get_user_model()
+
+class EmailAuthBackend(ModelBackend):
+    """
+    Email Authentication Backend    Permite a un usuario acceder utilizando su correo electrónico y
+    contraseña. Si la autenticación falla, lo intenta con el par
+    usuario/contraseña.
+    """
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(username=username)
+                if user.check_password(password):
+                    return user
+            except User.DoesNotExist:
+                return None
+    def get_user(self, user_id):
+       
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
