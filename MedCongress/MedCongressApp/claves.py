7c19ebb0e90bae59d242e17dd628273ae7e5368a
Dennis <dennis.molinetg@gmail.com>
2021-01-22 17:37:21 -0500

    actualizar

--- MedCongress/MedCongressApp/claves.py
+++ MedCongress/MedCongressApp/claves.py
@@ -1,18 +1,17 @@
-ID_KEY='m6ftsapwjvmo7j7y8mop'
-PRIVATE_KEY='sk_34664e85b5504ca39cc19d8f9b8df8a2'
-PUBLIC_KEY='pk_0d4449445a4948899811cea14a469793'
-URL_API='sandbox-api.openpay.mx'
-# URL_SITE='http://medcongress.softok2.mx'
-URL_SITE='http://localhost:8000'
-URL_PDF='dashboard.openpay.mx'
-PRUEBA=True
-
-
-# ID_KEY='mrkdgemoa3sig3imqehg'
-# PRIVATE_KEY='sk_a0db575d7bfb4233aed1e56eefaea045'
-# PUBLIC_KEY='pk_4b80b14008aa42618c4b0a9370539a85'
-# URL_API='api.openpay.mx'
-# URL_SITE='https://medcongress.com.mx'
-# # URL_SITE='http://localhost:8000'
+# ID_KEY='m6ftsapwjvmo7j7y8mop'
+# PRIVATE_KEY='sk_34664e85b5504ca39cc19d8f9b8df8a2'
+# PUBLIC_KEY='pk_0d4449445a4948899811cea14a469793'
+# URL_API='sandbox-api.openpay.mx'
+# # URL_SITE='http://medcongress.softok2.mx'
+# URL_SITE='http://localhost:8000'
 # URL_PDF='dashboard.openpay.mx'
-# PRUEBA=False    
+# PRUEBA=True
+
+ID_KEY='mrkdgemoa3sig3imqehg'
+PRIVATE_KEY='sk_a0db575d7bfb4233aed1e56eefaea045'
+PUBLIC_KEY='pk_4b80b14008aa42618c4b0a9370539a85'
+URL_API='api.openpay.mx'
+URL_SITE='https://medcongress.com.mx'
+# URL_SITE='http://localhost:8000'
+URL_PDF='dashboard.openpay.mx'
+PRUEBA=False    
