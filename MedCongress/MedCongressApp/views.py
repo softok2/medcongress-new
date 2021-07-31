83d9bee0a8953f9b8b787350215075a42c9a3303
Dennis <dennis.molinetg@gmail.com>
2021-07-30 21:59:48 -0400

    fecha actual en programa

--- MedCongress/MedCongressApp/views.py
+++ MedCongress/MedCongressApp/views.py
@@ -579,8 +579,12 @@ class CongresoDetail(TemplateView):
            
             context['fecha_ponencias']= data
             ponencias_env=[] 
-           
-            for dat in context['fecha_ponencias'] :
+            act_fecha=1
+            cont=0
+            for dat in context['fecha_ponencias']:
+                cont=cont+1 
+                if str(dat) == str(datetime.today().strftime('%Y-%m-%d')):  
+                    act_fecha=cont
                 bloques=Bloque.objects.filter(fecha_inicio__date=dat,congreso=congreso,published=True).order_by('fecha_inicio')
                 ponencias=Ponencia.objects.filter(fecha_inicio__date=dat,congreso=congreso,published=True,bloque=None).order_by('fecha_inicio')
                 talleres=Taller.objects.filter(fecha_inicio__date=dat,congreso=congreso,published=True,bloque=None).order_by('fecha_inicio')
@@ -657,7 +661,7 @@ class CongresoDetail(TemplateView):
                 #     ponentes_env.append(Taller.objects.filter(reltallerponente__pk=taller.id).distinct()) 
              
                 #     ponencias_env.append(talleres)
-          
+            context['act_fecha']=act_fecha
             context['ponencias']=ponencias_env
     
             prueba_ponecia=Ponencia.objects.filter(congreso=congreso.pk,published=True)
